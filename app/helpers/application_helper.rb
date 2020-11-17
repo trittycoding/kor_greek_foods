5fd6432c9eb73bcfde61e28a87a287ae10ba4973
trittycoding91 <trittycoding91@icloud.com>
2020-11-16 20:42:00 -0800

    Added jbuild_props method to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def jbuild_props(template, locals)
+    JbuilderTemplate.new(self) do |json|
+      json.partial!(partial: template, formats: [:json], handlers: [:jbuilder], locals: locals)
+    end.attributes!
+  end
 end
