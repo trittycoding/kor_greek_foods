bf4345d3e7bce0c271c399cded1b610251fe1832
trittycoding91 <trittycoding91@icloud.com>
2020-11-24 10:52:30 -0800

    Added Kor logo to center hero banner

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :customer_provinces
-  validates :distributor, :email, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
   validates :customer_province_id, presence: true
 end
