a550a5d7d3c97cbc6d05fafb13dc246adc3bb05e
trittycoding91 <trittycoding91@icloud.com>
2020-11-30 21:52:50 -0800

    Added active admin resource Productorder, stripe payments are processed however orders are not being created

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,10 +1,13 @@
 class CheckoutController < ApplicationController
+  pst_tax = 0
+  gst_tax = 0
+  hst_tax = 0
   def create
-    user_province = Province.find_by(province_id: current_user.province_id)
+    user_province = Province.find_by(id: current_user.province_id)
     province_gst = user_province.gst
     province_hst = user_province.hst
-    province_pst = user_province.province_pst
-    province_taxrate = user_province.taxrate
+    province_pst = user_province.pst
+    @province_taxrate = user_province.taxrate
 
     # Validations for cart items
     session[:user_cart].each do |item|
@@ -19,18 +22,18 @@ class CheckoutController < ApplicationController
     end
 
     # Purchasing valid items
+    @total = 0
     valid_products = []
-    total = 0
     session[:user_cart].each do |valid_item|
-      valid_product = Product.find(valid_item.to_i)
+      valid_product = Product.find_by(id: valid_item)
       valid_product_quantity = session[:item_quantity][valid_item.to_s]
-      valid_product_cost = (valid_product.price * 100).to_i
-      total += valid_product_cost
+      valid_product_cost = valid_product.price
+      @total += valid_product_cost
       valid_products <<
         {
           name: valid_product.name,
           description: valid_product.description,
-          amount: valid_product_cost,
+          amount: (valid_product_cost * 100).to_i,
           currency: 'cad',
           quantity: valid_product_quantity
         }
@@ -38,41 +41,45 @@ class CheckoutController < ApplicationController
 
     # Calculating tax
     unless province_hst.nil?
-      hst_tax = total * (province_hst * 100).to_i
+      @hst_tax = @total * province_hst
+      @total += @hst_tax
       valid_products <<
         {
           name: 'HST',
           description: 'Harmonized Sales Tax',
-          amount: hst_tax,
+          amount: (@hst_tax * 100).to_i,
           currency: 'cad',
           quantity: 1
         }
     end
 
     unless province_gst.nil?
-      gst_tax = total * (province_gst * 100).to_i
+      @gst_tax = @total * province_gst
+      @total += @gst_tax
       valid_products <<
         {
           name: 'GST',
           description: 'General Sales Tax',
-          amount: gst_tax,
+          amount: (@gst_tax * 100).to_i,
           currency: 'cad',
           quantity: 1
         }
     end
 
-    unless province.pst.nil?
-      pst_tax = total * (province_pst * 100).to_i
+    unless province_pst.nil?
+      @pst_tax = @total * province_pst
+      @total += @pst_tax
       valid_products <<
         {
           name: 'PST',
           description: 'Provincial Sales Tax',
-          amount: pst_tax,
+          amount: (@pst_tax * 100).to_i,
           currency: 'cad',
           quantity: 1
         }
     end
 
+    # Establishing a connection with Stripe
     # Establishing a connection with Stripe
     @session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
@@ -80,6 +87,7 @@ class CheckoutController < ApplicationController
       cancel_url: checkout_cancel_url,
       line_items: valid_products
     )
+
     # Redirecting the user to a payment screen via JS view
     respond_to do |format|
       format.js # render app/views/checkout/create.js.erb
@@ -87,12 +95,40 @@ class CheckoutController < ApplicationController
   end
 
   def success
-    # Payment accepted
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
-    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+    payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+    paid = payment_intent[:paid]
+
+    if paid
+      paid_order = Order.create(total: @total, user_id: current_user.id,
+                                gst: @gst_tax, hst: @hst_tax, pst: @pst_tax, taxrate: @province_taxrate,
+                                paid: true)
+
+      session[:user_cart].each do |x|
+        quantity_for_item = session[:item_quantity][x.to_s]
+        ProductOrder.create(order_id: paid_order.id, product_id: x.to_i,
+                            unitcost: x.price, quantity: quantity_for_item)
+      end
+
+    else
+      Order.create(total: @total, user_id: current_user.id,
+                   gst: @gst_tax, hst: @hst_tax, pst: @pst_tax, taxrate: @province_taxrate,
+                   paid: false)
+    end
+    session[:user_cart] = []
+    session[:item_quantity] = {}
   end
 
   def cancel
-    # Payment did not go through
+    return_from_session = Stripe::Checkout::Session.retrieve(params[:session_id])
+    payment_intent = Stripe::PaymentIntent.retrieve(return_from_session.session.payment_intent)
+    paid = payment_intent[:paid]
+    unless paid?
+      Order.create(total: @total, user_id: current_user.id,
+                   gst: @gst_tax, hst: @hst_tax, pst: @pst_tax, taxrate: @province_taxrate,
+                   paid: false)
+    end
+    session[:user_cart] = []
+    session[:item_quantity] = {}
   end
 end
